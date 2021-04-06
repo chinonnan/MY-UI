2a5bf88b2182e7c8f77557a3af3c1251dbe65144
陳eN <634670865@qq.com>
2021-04-06 21:18:25 +0800

    完成按钮基本样样

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 作者:jhon titor
 
 
+## 安装
+
+使用本框架前，请在CSS中开启border-box
+```
+*{box-sizing:border-box;}
+```
---
+++ button.js
@@ -0,0 +1,5 @@
+Vue.component('my-button',{
+    template:`
+    <button class="g-button">按钮</button>
+    `
+})
