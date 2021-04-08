ef3b14cb18eac62011b17c52af4d7cac13a1711c
陳eN <634670865@qq.com>
2021-04-08 13:39:12 +0800

    完成button group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('my-button',Button)
 Vue.component('my-icon',Icon)
+Vue.component('my-button-group',ButtonGroup)
 new Vue({
     el: "#app",
     data:{
